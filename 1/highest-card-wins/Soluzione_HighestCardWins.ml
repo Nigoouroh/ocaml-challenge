0e1b754ae413ae32c3848d64c27ac252a006bdb2
Nigoouroh <pibinicolo@gmail.com>
2025-10-22 21:33:25 +0200

    Risolto conflitto dello stash

---
+++ 1/highest-card-wins/Soluzione_HighestCardWins.ml
@@ -0,0 +1,9 @@
+type card = Joker | Val of int;;
+
+let win c1 c2 = 
+  match(c1, c2) with
+    (Val a, Val b) -> (a > b) 
+  | (_, Joker) -> false
+  | (Joker, _) -> true;;
+
+assert (win (Joker) (Val 3) = true);;
